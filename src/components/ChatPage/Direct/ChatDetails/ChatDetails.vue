64eae9f353064eeef37c6cdee73ff484ef6c01eb
sandberg-smith <sandbergjacques500@gmail.com>
2020-12-05 12:53:12 +0200

    did tab to choose files to see

--- src/components/ChatPage/Direct/ChatDetails/ChatDetails.vue
+++ src/components/ChatPage/Direct/ChatDetails/ChatDetails.vue
@@ -30,61 +30,18 @@
               </div>
             </div>
             <hr class="m-3" />
-            <div class="recent-atachments p-4">
-               <v-tabs>
-                <v-tab>Photos &amp; media <span class="num rounded rounded-full"></span> </v-tab>
-                <v-tab>Documents<span class="num rounded rounded-full"></span> </v-tab>
-              </v-tabs>
-              <ul>
-                <li class="text-gray-700 p-2">
-                  <svg class="svg-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32"><path fill="none" d="M0 0h24v24H0z"/><path d="M20 22H4a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1zm-1-2V4H5v16h14zm-5-8v5h-4v-3h2v-2h2zm-2-8h2v2h-2V4zm-2 2h2v2h-2V6zm2 2h2v2h-2V8zm-2 2h2v2h-2v-2z" fill="rgba(18,184,0,1)"/></svg>
-                  Making_more_money.zip
-                </li>
-                <li class="text-gray-700 p-2">
-                  <svg class="svg-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32"><path fill="none" d="M0 0h24v24H0z"/><path d="M20 22H4a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1zm-1-2V4H5v16h14zm-5-8v5h-4v-3h2v-2h2zm-2-8h2v2h-2V4zm-2 2h2v2h-2V6zm2 2h2v2h-2V8zm-2 2h2v2h-2v-2z" fill="rgba(18,184,0,1)"/></svg>
-                  Making_more_money.zip
-                </li>
-                <li class="text-gray-700 p-2">
-                  <svg class="svg-icon" xmlns="http://www.w3.org/2000/svg" width="24.886" height="22.472" viewBox="0 0 24.886 22.472">
-                    <path id="Icon_simple-microsoftword" data-name="Icon simple-microsoftword" d="M24.886,4.017V20.379a.434.434,0,0,1-.155.342.541.541,0,0,1-.373.136H15.483V18.588h7.24V17.555H15.477V16.29h7.24V15.258H15.482V13.982H22.72v-1.02H15.482V11.683H22.72V10.651H15.482V9.386H22.72V8.355H15.482V7.064H22.72V6.082H15.482V3.518H24.36a.479.479,0,0,1,.371.147.449.449,0,0,1,.155.352ZM14.155.959V23.427L0,21.092V3.358L14.155.956ZM12.018,7.592l-1.772.1L9.11,14.405H9.084c-.056-.317-.264-1.429-.638-3.329L7.783,7.85,6.12,7.929l-.666,3.147c-.389,1.833-.606,2.9-.663,3.2H4.776L3.765,8.1l-1.524.077,1.633,7.795,1.694.1.638-3.034c.373-1.78.591-2.814.638-3.1h.047c.063.3.265,1.359.638,3.174l.638,3.123,1.835.1L12.055,7.59h-.036Z" transform="translate(0 -0.956)" fill="#0049dc"/>
-                  </svg>
-                  SDL_slides_unit5.docx
-                </li>
-                <li class="text-gray-700 p-2">
-                  <svg class="svg-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32"><path fill="none" d="M0 0h24v24H0z"/><path d="M17 3h4a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1h-4V3zM2.859 2.877l12.57-1.795a.5.5 0 0 1 .571.495v20.846a.5.5 0 0 1-.57.495L2.858 21.123a1 1 0 0 1-.859-.99V3.867a1 1 0 0 1 .859-.99zM5 8v8h2v-2h6V8H5zm2 2h4v2H7v-2z" fill="rgba(231,76,60,1)"/></svg>
-                  SDL_slides_unit5.docx
-                </li>
-              </ul>
-              <div class="images gap-4">
-                <div class="shared-image">
-                  <img src="../../../../assets/images/avatar1.jpg" alt="you sent this image to Gashumba on 28 Jan 2020">
+            <div class="recent-atachments px-4">
+              <div class="flex justify-between mb-3">
+                <div :class="(this.fileToShow == 'media' ? 'active ':'') + ' files hover:bg-blue-100 p-3 '">
+                  <button @click="changeFileToShow('media')">Photos &amp; media</button>                    
                 </div>
-                <div class="shared-image">
-                  <img src="../../../../assets/images/avatar1.jpg" alt="you sent this image to Gashumba on 28 Jan 2020">
+                <div :class="(this.fileToShow == 'docs' ? 'active ':'') + 'files hover:bg-blue-100 p-3'">
+                  <button @click="changeFileToShow('docs')"> Documents</button>
                 </div>
-                <div class="shared-image">
-                  <img src="../../../../assets/images/avatar1.jpg" alt="you sent this image to Gashumba on 28 Jan 2020">
-                </div>
-                
-                <div class="shared-image">
-                  <img src="../../../../assets/images/avatar1.jpg" alt="you sent this image to Gashumba on 28 Jan 2020">
-                </div>
-                <div class="shared-image">
-                  <img src="../../../../assets/images/avatar1.jpg" alt="you sent this image to Gashumba on 28 Jan 2020">
-                </div>
-                <div class="shared-image">
-                  <img src="../../../../assets/images/avatar1.jpg" alt="you sent this image to Gashumba on 28 Jan 2020">
-                </div>
-                
-                <div class="shared-image">
-                  <img src="../../../../assets/images/avatar1.jpg" alt="you sent this image to Gashumba on 28 Jan 2020">
-                </div>
-                <div class="shared-image">
-                  <img src="../../../../assets/images/avatar1.jpg" alt="you sent this image to Gashumba on 28 Jan 2020">
-                </div>
-                <div class="shared-image">
-                  <img src="../../../../assets/images/avatar1.jpg" alt="you sent this image to Gashumba on 28 Jan 2020">
-                </div>
+              </div>
+              <div class="real-files">
+                  <SharedDocs v-if="this.fileToShow == 'docs'" />
+                  <SharedImages v-if="this.fileToShow == 'media'" />
               </div>
             </div>
         </div>
@@ -95,7 +52,24 @@
 
 <script>
 export default {
-  name: "ChatDetails",
+  name: "ChatDetails", 
+  components:{
+    SharedDocs: ()=> import("./files/SharedDocs"),
+    SharedImages: ()=> import("./files/SharedImages")
+  },
+  data() {
+    return {
+      fileToShow: "docs"
+    };
+  },
+  methods: {
+      /*
+       @param {String} fileType 
+      */
+    changeFileToShow(fileType){
+      this.fileToShow = fileType
+    }
+  }
 };
 </script>
 
@@ -148,26 +122,11 @@ export default {
   border: 3px solid white;
   background-color: rgb(0, 0, 0,0.1);
 }
-.svg-icon{
-  display: inline-block;
-  margin-right: 4px;
+.files.active{
+  border:none;
+  border-bottom: 2px solid rgba(66, 153, 225,1);
 }
-.images{
-  display: flex;
-  flex-wrap: wrap;
+button{
+  outline: none !important;
 }
-
-.images .shared-image{
-  width:40% !important;
-}
-
-.shared-image img{
-  display:block;
-  max-width: 100%;
-  border-radius: 10px;
-}
-/* span.num{
-  padding:7px;
-  background: #4a81e2;
-} */
 </style>
