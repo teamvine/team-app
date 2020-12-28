3e5473bd99ee52d42645ba38a40e570edbbe78b5
Harerimana Egide <egideharerimana085@gmail.com>
2020-12-28 11:58:20 +0200

    added netlify config

--- src/router/index.js
+++ src/router/index.js
@@ -116,7 +116,7 @@ const routes = [
     component: () => import(/* webpackChunkName: "email_verification" */ '../pages/CodeVerification.vue')
   },
   {
-    path: '**',
+    path: '/**',
     name: 'NotFound',
     component: () => import(/* webpackChunkName: "register" */ '../pages/NotFound.vue')
   },
