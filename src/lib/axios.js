9cc7f4cfe59da311f1fc37142e5868c1cfcd42cb
Harerimana Egide <egideharerimana085@gmail.com>
2021-01-01 17:42:52 +0200

    removed nprogress on axios request

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,16 +4,16 @@ const axios = require("axios");
 const instance = axios.default.create();
 
 // before a request is made start the nprogress
-instance.interceptors.request.use((config) => {
-  NProgress.start();
-  return config;
-});
+// instance.interceptors.request.use((config) => {
+//   NProgress.start();
+//   return config;
+// });
 
 instance.interceptors
 // before a response is returned stop nprogress
-instance.interceptors.response.use((response) => {
-  NProgress.done();
-  return response;
-});
+// instance.interceptors.response.use((response) => {
+//   NProgress.done();
+//   return response;
+// });
 
 module.exports = instance;
