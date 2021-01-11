8d9e90a220e5318eada25cb8d9409ca9851467d1
sandberg-smith <sandbergjacques500@gmail.com>
2021-01-11 08:07:26 +0200

    finished add member to channel modal

--- src/components/ChatPage/Channel/ChatDetails/ChatDetails.vue
+++ src/components/ChatPage/Channel/ChatDetails/ChatDetails.vue
@@ -147,7 +147,7 @@
         </div>
         
     <vue-modal style="z-index: 2500" class="py-6 col-lg-9" name="addMembers" :scrollable="true" draggable=".drag-handler" height="auto" :reset="true" :adaptive="true">
-      <nav class="flex drag-handler border-b items-center justify-between flex-wrap bg-teal p-6 py-2">
+      <nav class="flex drag-handler items-center justify-between flex-wrap bg-teal p-6 py-2">
         <div class="flex items-center flex-no-shrink text-black mr-6">
           <!-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="35" height="35"><path fill="none" d="M0 0h24v24H0z"/><path d="M2 8.994A5.99 5.99 0 0 1 8 3h8c3.313 0 6 2.695 6 5.994V21H8c-3.313 0-6-2.695-6-5.994V8.994zM20 19V8.994A4.004 4.004 0 0 0 16 5H8a3.99 3.99 0 0 0-4 3.994v6.012A4.004 4.004 0 0 0 8 19h12zm-6-8h2v2h-2v-2zm-6 0h2v2H8v-2z"/></svg> -->
           <span class="text-2xl tracking-tight ml-3 font-bold">Add Members</span>
