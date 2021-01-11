7b0bbd50c0110e98d54dda779d4b3caa9bc03840
Harerimana Egide <egideharerimana085@gmail.com>
2021-01-11 09:28:21 +0200

    removed click issues

--- src/components/ChatPage/Channel/ChatDetails/ChatDetails.vue
+++ src/components/ChatPage/Channel/ChatDetails/ChatDetails.vue
@@ -17,7 +17,7 @@
                 <div class="w-full mb-4 px-3">
                     <div x-data={show:false} class="rounded-sm">
                         <div class="px-5 py-4 headingOne">
-                            <button @click="show=!show" class="btn-itm text-black-300 font-bold focus:outline-none" type="button">
+                            <button class="btn-itm text-black-300 font-bold focus:outline-none" type="button">
                                 <svg class="icn1" xmlns="http://www.w3.org/2000/svg" style="display: inline-block" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm-1-11v6h2v-6h-2zm0-4v2h2V7h-2z"/></svg>
                                 <b class="font-bold ml-2 text-md">About #{{currentChannel.name}}</b>
                                 <svg class="icn" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="33" height="33"><path fill="none" d="M0 0h24v24H0z"/><path d="M12.172 12L9.343 9.172l1.414-1.415L15 12l-4.243 4.243-1.414-1.415z"/></svg>
@@ -29,7 +29,7 @@
                     </div>
                     <div x-data={show:false} class="rounded-sm">
                         <div class="px-5 py-4 headingOne">
-                            <button @click="show=!show" class="btn-itm text-black-300 font-bold focus:outline-none" type="button">
+                            <button class="btn-itm text-black-300 font-bold focus:outline-none" type="button">
                                 <!-- <svg class="icn1" xmlns="http://www.w3.org/2000/svg" style="display: inline-block" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M2 22a8 8 0 1 1 16 0H2zm8-9c-3.315 0-6-2.685-6-6s2.685-6 6-6 6 2.685 6 6-2.685 6-6 6zm7.363 2.233A7.505 7.505 0 0 1 22.983 22H20c0-2.61-1-4.986-2.637-6.767zm-2.023-2.276A7.98 7.98 0 0 0 18 7a7.964 7.964 0 0 0-1.015-3.903A5 5 0 0 1 21 8a4.999 4.999 0 0 1-5.66 4.957z"/></svg> -->
                                 <svg class="icn1" style="display: inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M20 22h-2v-2a3 3 0 0 0-3-3H9a3 3 0 0 0-3 3v2H4v-2a5 5 0 0 1 5-5h6a5 5 0 0 1 5 5v2zm-8-9a6 6 0 1 1 0-12 6 6 0 0 1 0 12zm0-2a4 4 0 1 0 0-8 4 4 0 0 0 0 8z"/></svg>
                                 <b class="font-bold ml-2 text-md">Channel Members</b>
@@ -54,7 +54,7 @@
                     </div>
                     <div x-data={show:false} class="rounded-sm">
                         <div class="px-5 py-4 headingOne">
-                            <button @click="show=!show" class="btn-itm text-black-300 font-bold focus:outline-none" type="button">
+                            <button class="btn-itm text-black-300 font-bold focus:outline-none" type="button">
                                 <svg class="icn1" xmlns="http://www.w3.org/2000/svg" style="display: inline-block" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M16 4a1 1 0 0 1 1 1v4.2l5.213-3.65a.5.5 0 0 1 .787.41v12.08a.5.5 0 0 1-.787.41L17 14.8V19a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h14zm-1 2H3v12h12V6zM7.4 8.829a.4.4 0 0 1 .215.062l4.355 2.772a.4.4 0 0 1 0 .674L7.615 15.11A.4.4 0 0 1 7 14.77V9.23c0-.221.18-.4.4-.4zM21 8.84l-4 2.8v.718l4 2.8V8.84z"/></svg>
                                 <b class="font-bold ml-2 text-md">Start A Meeting</b>
                                 <svg class="icn" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="33" height="33"><path fill="none" d="M0 0h24v24H0z"/><path d="M12.172 12L9.343 9.172l1.414-1.415L15 12l-4.243 4.243-1.414-1.415z"/></svg>
@@ -66,7 +66,7 @@
                     </div>
                     <div x-data={show:false} class="rounded-sm">
                         <div class="px-5 py-4 headingOne">
-                            <button @click="show=!show" class="btn-itm text-black-300 font-bold focus:outline-none" type="button">
+                            <button class="btn-itm text-black-300 font-bold focus:outline-none" type="button">
                                 <svg class="icn1" xmlns="http://www.w3.org/2000/svg" style="display: inline-block" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M22 12.999V20a1 1 0 0 1-1 1h-8v-8.001h9zm-11 0V21H3a1 1 0 0 1-1-1v-7.001h9zM11 3v7.999H2V4a1 1 0 0 1 1-1h8zm10 0a1 1 0 0 1 1 1v6.999h-9V3h8z"/></svg>
                                 <b class="font-bold ml-2 text-md">Connected Apps</b>
                                 <svg class="icn" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="33" height="33"><path fill="none" d="M0 0h24v24H0z"/><path d="M12.172 12L9.343 9.172l1.414-1.415L15 12l-4.243 4.243-1.414-1.415z"/></svg>
@@ -78,7 +78,7 @@
                     </div>
                     <div x-data={show:false} class="rounded-sm">
                         <div class="px-5 py-4 headingOne border-b">
-                            <button @click="show=!show" class="btn-itm text-black-300 font-bold focus:outline-none" type="button">
+                            <button class="btn-itm text-black-300 font-bold focus:outline-none" type="button">
                                 <svg class="icn1" xmlns="http://www.w3.org/2000/svg" style="display: inline-block" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M22 11v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-9h20zm0-2H2V4a1 1 0 0 1 1-1h7.414l2 2H21a1 1 0 0 1 1 1v3z"/></svg>
                                 <b class="font-bold ml-2 text-md">Shared Files</b>
                                 <svg class="icn" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="33" height="33"><path fill="none" d="M0 0h24v24H0z"/><path d="M12.172 12L9.343 9.172l1.414-1.415L15 12l-4.243 4.243-1.414-1.415z"/></svg>
