894d5819af145820a3c86f715400e14d67afa81b
Harerimana Egide <egideharerimana085@gmail.com>
2021-02-11 11:02:36 +0200

    chnged dropdown

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -20,69 +20,52 @@
         </div>
         <div class="drop-down shadow-md" v-show="showDropDown" v-click-outside="outsideClick">
           <div
-            class="drop-content border origin-top-right absolute right-0 mt-2 w-full rounded-sm shadow-md bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100"
+            class="drop-content border origin-top-right absolute right-0 mt-2 w-full rounded-sm shadow-md bg-white ring-1 ring-black ring-opacity-5"
             role="menu"
             aria-orientation="vertical"
             aria-labelledby="options-menu">
-            <div class="flex flex-wrap overflow-hidden">
+            <div class="">
               <div class="px-6 mt-2 hover:bg-white d-block w-full pt-0">
-                <div class="font-bold text-md text-center border-0 d-block hover:bg-white tracking-wide w-full text-c2 mb-4">
+                <div class="font-bold text-lg text-center border-0 d-block hover:bg-white tracking-wide w-full text-c2 mb-4">
                   {{user.full_name||''}}
                 </div>
-                <div
-                  @click="navigate({name: 'Start'})"
-                  class="flex d-block item-hover cursor-pointer w-full px-4 py-2 font-bold text-md text-grey-darkest border-b-0"
-                >
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-                    <path fill="none" d="M0 0h24v24H0z" />
-                    <path d="M19 20a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1v-9H0l10.327-9.388a1 1 0 0 1 1.346 0L22 11h-3v9zm-8-5a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"/>
-                  </svg>
-                  <div class="pl-2 text-md font-bold">Go Home</div>
-                </div>
-                <div
-                  @click="navigate({name: 'MyAccount'})"
-                  class="flex item-hover d-block cursor-pointer px-4 py-2 text-md text-grey-darkest border-b-0">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-                    <path fill="none" d="M0 0h24v24H0z" />
-                    <path d="M14 21l-2 2-2-2H4.995A1.995 1.995 0 0 1 3 19.005V4.995C3 3.893 3.893 3 4.995 3h14.01C20.107 3 21 3.893 21 4.995v14.01A1.995 1.995 0 0 1 19.005 21H14zm-7.643-3h11.49a6.992 6.992 0 0 0-5.745-3 6.992 6.992 0 0 0-5.745 3zM12 13a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
-                  </svg>
-                  <div class="pl-2 text-md font-bold">My Account</div>
-                </div>
-                <div class="d-block item-hover flex cursor-pointer px-4 py-2 text-md text-grey-darkest" >
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-                    <path fill="none" d="M0 0h24v24H0z" />
-                    <path d="M20 18.667l.4.533a.5.5 0 0 1-.4.8H4a.5.5 0 0 1-.4-.8l.4-.533V10a8 8 0 1 1 16 0v8.667zM9.5 21h5a2.5 2.5 0 1 1-5 0z"/>
-                  </svg>
-                  <div class="pl-2 text-md font-bold">Notifications</div>
-                </div>
-                <div
-                  v-if="userAppFlow.switchedWorkspaces"
-                  @click="navigate({name: 'ChannelChat',params: {channel_code: currentWorkspaceJoinedChannels.find(channel=> channel.gen==true).channel_code}})"
-                  class="d-block item-hover flex cursor-pointer px-4 py-2 text-md text-grey-darkest">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-                    <path fill="none" d="M0 0h24v24H0z" />
-                    <path d="M14.45 19L12 22.5 9.55 19H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1h-6.55z"/>
-                  </svg>
-                  <div class="pl-2 text-md font-bold">Messages</div>
-                </div>
-                <div
-                  @click="navigate({name: 'SettingsPage'})"
-                  class="item-hover d-block flex cursor-pointer border-bottom px-4 py-2 text-md text-grey-darkest">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-                    <path fill="none" d="M0 0h24v24H0z" />
-                    <path d="M9.954 2.21a9.99 9.99 0 0 1 4.091-.002A3.993 3.993 0 0 0 16 5.07a3.993 3.993 0 0 0 3.457.261A9.99 9.99 0 0 1 21.5 8.876 3.993 3.993 0 0 0 20 12c0 1.264.586 2.391 1.502 3.124a10.043 10.043 0 0 1-2.046 3.543 3.993 3.993 0 0 0-3.456.261 3.993 3.993 0 0 0-1.954 2.86 9.99 9.99 0 0 1-4.091.004A3.993 3.993 0 0 0 8 18.927a3.993 3.993 0 0 0-3.457-.26A9.99 9.99 0 0 1 2.5 15.121 3.993 3.993 0 0 0 4 11.999a3.993 3.993 0 0 0-1.502-3.124 10.043 10.043 0 0 1 2.046-3.543A3.993 3.993 0 0 0 8 5.071a3.993 3.993 0 0 0 1.954-2.86zM12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-                  </svg>
-                  <div class="pl-2 text-md font-bold">Change Settings</div>
-                </div>
-                <div
-                  @click="onLogOut"
-                  @dblclick="onLogOut"
-                  class="item-hover d-block flex font-bold cursor-pointer text-left px-2 py-0 text-md text-grey-darkest">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" class="ml-3">
-                    <path fill="none" d="M0 0h24v24H0z" />
-                    <path fill="orangered" d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2a9.985 9.985 0 0 1 8 4h-2.71a8 8 0 1 0 .001 12h2.71A9.985 9.985 0 0 1 12 22zm7-6v-3h-8v-2h8V8l5 4-5 4z"/>
-                  </svg>
-                  <div class="pl-2 font-bold py-3">Sign Out</div>
+              <div class="block space-y-2 text-md w-full pr-0 lst">
+                        <router-link to="/" :class="[$route.name=='MyAccount'? 'bg-indigo-100':'']" class="space-x-3 text-gray-900 w-full p-1 rounded-md hover:bg-indigo-100 focus:bg-indigo-100 d-block">
+                          <svg class="h-5" style="display: inline-block !important;" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                          </svg>
+                          <span class="font-bold h-5 ">Home</span>
+                        </router-link>
+                        <router-link to="/settings/account" :class="[$route.name=='MyAccount'? 'bg-indigo-100':'']" class="space-x-3 text-gray-900 w-full p-1 rounded-md hover:bg-indigo-100 focus:bg-indigo-100 d-block">
+                          <svg class="h-5" style="display: inline-block !important;" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                          </svg>
+                          <span class="font-bold h-5 ">My Account</span>
+                        </router-link>
+                        <a href="#" :class="[$route.name=='NotificationSettings'? 'bg-indigo-100':'']" class="space-x-3 text-gray-900 w-full p-1 rounded-md hover:bg-indigo-100 focus:bg-indigo-100 d-block">
+                          <svg class="h-5" style="display: inline-block !important;"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path>
+                          </svg>
+                          <span class="font-bold">Notifications</span>
+                        </a>
+                        <router-link :to="'/chat/channel/'+currentWorkspaceJoinedChannels.find(channel=> channel.gen==true).channel_code" class="space-x-3 text-gray-900 w-full p-1 rounded-md hover:bg-indigo-100 focus:bg-indigo-100 d-block">
+                          <svg class="h-5" style="display: inline-block !important;"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path>
+                          </svg>
+                          <span class="font-bold">Messages</span>
+                        </router-link>
+                        <router-link to="/settings/profile-settings" class="space-x-3 text-gray-900 w-full p-1 rounded-md hover:bg-indigo-100 focus:bg-indigo-100 d-block">
+                          <svg class="h-5" style="display: inline-block !important;"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path>
+                          </svg>
+                          <span class="font-bold">Settings</span>
+                        </router-link>
+                        <p @click="onLogOut" @dblclick="onLogOut" class="space-x-3 text-gray-900 w-full p-1 rounded-md hover:bg-indigo-100 focus:bg-indigo-100 d-block">
+                          <svg class="h-5" style="display: inline-block !important;"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+                          </svg>
+                          <span class="font-bold">Logout</span>
+                        </p>
                 </div>
               </div>
             </div>
@@ -134,9 +117,10 @@ export default {
 </script>
 
 <style scoped>
-.d-block {
+.lst,.d-block {
   display: block !important;
   flex: none;
+  width: 100%;
 }
 .item-hover:hover {
   background-color: rgb(241, 245, 252);
@@ -260,7 +244,7 @@ export default {
   z-index: 1;
 }
 .drop-content {
-  min-width: 300px;
+  min-width: 310px;
   border-radius: 5px;
 }
 .drop-content * {
@@ -276,20 +260,10 @@ export default {
   margin-right: 2%;
 }
 .drop-content div div:nth-child(1) span {
-  font-size: 18px;
   height: 100%;
   padding: 2% 0;
   width: 60%;
   font-weight: bold;
-}
-.drop-content div div:nth-child(1) svg {
-  margin-right: 0px !important;
-}
-.drop-content div div:not(:nth-child(1)) svg {
-  display: inline-block;
-  margin-right: 2%;
-  fill: #2f74eb;
-  fill: linear-gradient(180deg, #2f74eb 0%, #83eaf1 74%);
 }
 .user-pic {
   width: 40px;
