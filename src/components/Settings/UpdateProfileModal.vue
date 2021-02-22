df33e57f0f57bf978564248f0907898e7e305f70
Harerimana Egide <egideharerimana085@gmail.com>
2021-02-22 21:18:36 +0200

    started profile modal

--- src/components/Settings/UpdateProfileModal.vue
+++ src/components/Settings/UpdateProfileModal.vue
@@ -1,22 +1,36 @@
 <template>
-  <t-modal header="Title of the modal">
-    Content of the modal.
-    <template v-slot:footer>
-        <div class="flex justify-between">
-            <t-button type="button">
-                Cancel
-            </t-button>
-            <t-button type="button">
-                Ok
-            </t-button>
-        </div>
-    </template>
-</t-modal>
+    <div>
+        <t-modal :value="showModal" header="Title of the modal">
+            <template v-slot:header>
+                <h1>Profile Picture</h1>
+            </template>
+            Content of the modal.
+            <template v-slot:footer>
+                <div class="flex justify-between">
+                    <t-button type="button">
+                        Cancel
+                    </t-button>
+                    <t-button type="button">
+                        Ok
+                    </t-button>
+                </div>
+            </template>
+        </t-modal>
+    </div>
 </template>
 
 <script>
 export default {
-    name: 'UpdateProfileModal'
+    name: 'UpdateProfileModal',
+    props: {
+        showModal: Boolean,
+        onClose: Function
+    },
+    data(){
+        return {
+
+        }
+    }
 }
 </script>
 
