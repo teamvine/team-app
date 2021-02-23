0bac8823d2cfe6c5a65ed221818d161b9a867ed9
Harerimana Egide <egideharerimana085@gmail.com>
2021-02-23 21:11:13 +0200

    added a new vuejs cropper that works well

--- src/components/Settings/ImageCropper.vue
+++ src/components/Settings/ImageCropper.vue
@@ -1,9 +1,17 @@
 <template>
-  <div>
+  <div class="block w-full">
+    <img
+      class="w-full"
+      style="display: block; max-width: 100% !important"
+      id="image_to_crop"
+      :src="image_data_uri"
+    />
   </div>
 </template>
 
 <script>
+import "cropperjs/dist/cropper.min.css";
+import Cropper from "cropperjs";
 export default {
   name: "ImageCropper",
   props: {
@@ -11,15 +19,35 @@ export default {
       type: String,
     },
   },
-  components: {
-  },
   data() {
-    return {};
+    return {
+      cropper: null,
+    };
   },
   methods: {
-    change() {
-    },
-  }
+    change() {},
+  },
+  mounted() {
+    this.cropper = new Cropper(this.$el.querySelector("#image_to_crop"), {
+      aspectRatio: 1 / 1,
+      viewMode: 1,
+      autoCrop: true,
+      dragMode: "move",
+      minCropBoxWidth: 100,
+      minCropBoxHeight: 100,
+      cropBoxResizable: true,
+      ready() {},
+      crop(event) {
+        console.log(event.detail.x);
+        console.log(event.detail.y);
+        console.log(event.detail.width);
+        console.log(event.detail.height);
+        console.log(event.detail.rotate);
+        console.log(event.detail.scaleX);
+        console.log(event.detail.scaleY);
+      },
+    });
+  },
 };
 </script>
 
