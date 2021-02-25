f9e52ca5fa4acd61d909b122ae354e71239c2cc5
Harerimana Egide <egideharerimana085@gmail.com>
2021-02-25 20:08:24 +0200

    ----Here everything is working well: RESTORE POINT

--- src/components/StartPage/Footer/Footer.vue
+++ src/components/StartPage/Footer/Footer.vue
@@ -94,54 +94,8 @@
         </div>
       </div>
     </div>
-    <div class="bg-indigo-100 p-2 py-4 pl-6 w-full text-center">
-      <span class="text-black text-sm">© Copyright 2020 - RCONNECT.Inc</span>
-      <button class="bg-grey-light hover:bg-grey text-grey-darkest ml-3 font-bold rounded inline-flex items-center">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          width="24"
-          height="24"
-          style="fill: purple;"
-        >
-          <path fill="none" d="M0 0h24v24H0z" />
-          <path
-            d="M12 2c2.717 0 3.056.01 4.122.06 1.065.05 1.79.217 2.428.465.66.254 1.216.598 1.772 1.153a4.908 4.908 0 0 1 1.153 1.772c.247.637.415 1.363.465 2.428.047 1.066.06 1.405.06 4.122 0 2.717-.01 3.056-.06 4.122-.05 1.065-.218 1.79-.465 2.428a4.883 4.883 0 0 1-1.153 1.772 4.915 4.915 0 0 1-1.772 1.153c-.637.247-1.363.415-2.428.465-1.066.047-1.405.06-4.122.06-2.717 0-3.056-.01-4.122-.06-1.065-.05-1.79-.218-2.428-.465a4.89 4.89 0 0 1-1.772-1.153 4.904 4.904 0 0 1-1.153-1.772c-.248-.637-.415-1.363-.465-2.428C2.013 15.056 2 14.717 2 12c0-2.717.01-3.056.06-4.122.05-1.066.217-1.79.465-2.428a4.88 4.88 0 0 1 1.153-1.772A4.897 4.897 0 0 1 5.45 2.525c.638-.248 1.362-.415 2.428-.465C8.944 2.013 9.283 2 12 2zm0 5a5 5 0 1 0 0 10 5 5 0 0 0 0-10zm6.5-.25a1.25 1.25 0 0 0-2.5 0 1.25 1.25 0 0 0 2.5 0zM12 9a3 3 0 1 1 0 6 3 3 0 0 1 0-6z"
-          />
-        </svg>
-      </button>
-      <button
-        class="bg-grey-light hover:bg-grey text-grey-darkest ml-3 font-bold rounded inline-flex items-center"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          width="24"
-          height="24"
-          style="fill:#0391FD;"
-        >
-          <path fill="none" d="M0 0h24v24H0z" />
-          <path
-            d="M22.162 5.656a8.384 8.384 0 0 1-2.402.658A4.196 4.196 0 0 0 21.6 4c-.82.488-1.719.83-2.656 1.015a4.182 4.182 0 0 0-7.126 3.814 11.874 11.874 0 0 1-8.62-4.37 4.168 4.168 0 0 0-.566 2.103c0 1.45.738 2.731 1.86 3.481a4.168 4.168 0 0 1-1.894-.523v.052a4.185 4.185 0 0 0 3.355 4.101 4.21 4.21 0 0 1-1.89.072A4.185 4.185 0 0 0 7.97 16.65a8.394 8.394 0 0 1-6.191 1.732 11.83 11.83 0 0 0 6.41 1.88c7.693 0 11.9-6.373 11.9-11.9 0-.18-.005-.362-.013-.54a8.496 8.496 0 0 0 2.087-2.165z"
-          />
-        </svg>
-      </button>
-      <button
-        class="bg-grey-light hover:bg-grey text-grey-darkest ml-3 font-bold rounded inline-flex items-center"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          width="24"
-          height="24"
-          style="fill: blue;"
-        >
-          <path fill="none" d="M0 0h24v24H0z" />
-          <path
-            d="M6.94 5a2 2 0 1 1-4-.002 2 2 0 0 1 4 .002zM7 8.48H3V21h4V8.48zm6.32 0H9.34V21h3.94v-6.57c0-3.66 4.77-4 4.77 0V21H22v-7.93c0-6.17-7.06-5.94-8.72-2.91l.04-1.68z"
-          />
-        </svg>
-      </button>
+    <div class="bg-footer-sm p-2 py-4 pl-6 w-full text-center">
+      <span class="text-white text-sm">© Copyright 2020 - RCONNECT.Inc</span>
     </div>
   </footer>
 </template>
@@ -159,8 +113,12 @@ export default {
       top: 100%
     }
     .footer-bg {
-      /* background-color: #1d2124; */
-      background-color: #3B5998;
+      /* background-color: #2B2C7C; */
+      background-color: #102C54;
+      /* background-color: #3B5998; */
+    }
+    .bg-footer-sm {
+      background-color: #0d223f;
     }
     .font-bold {
         font-weight: bolder;
